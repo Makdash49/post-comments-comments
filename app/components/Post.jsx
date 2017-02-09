97e9137174b2f88726a226a1221001801ea57275
Mark McQuillen <markdmcq@hotmail.com>
2017-02-09 10:48:23 -0800

    Add comment form and item list

--- app/components/Post.jsx
+++ app/components/Post.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
-// import AddItem from 'AddItem';
-// import ItemList from 'ItemList'
+import AddItem from 'AddItem';
+import ItemList from 'ItemList'
 
 export class Post extends React.Component {
 
@@ -19,6 +19,8 @@ export class Post extends React.Component {
           <h1>username</h1>
           <h2>POST TITLE + CONTENT</h2>
         </div>
+        <ItemList/>
+        <AddItem/>
         <button className="my-green-button float-right" onClick={this.handleAddComment.bind(this)}>Add Comment</button>
       </div>
     )
@@ -30,3 +32,6 @@ export default connect(
     return state;
   }
 )(Post);
+
+// What needs to happen?  I need to hit add comment and a form needs to appear.  We can do this with true or false. if AddPostComment === true
+// then render the component with a form.  Otherwise don't.
